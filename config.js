b3a31c138b2cca751d15feaa3af4655d7f28d5f9
ayoubsukasfsd-jpg <ayoubsukasfsd@gmail.com>
2026-07-04 11:53:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "wF8w0A4Z#HoUV8QuadTuBZaDGITfvWZwxZya4WR0yJO_0pTA0tT4",
+SESSION_ID: process.env.SESSION_ID || "ZctzTaAb#3KHGsUHWedMJ_dA3k-9RWbsTZocNNGDH6rJyctGRf6I",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
